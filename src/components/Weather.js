4b55653587df42c90f6662a9b9eca6ee28270f15
ashish <adhsashish125@gmail.com>
2024-05-01 22:40:22 +0545

    Integrated both APIS

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -62,26 +62,72 @@ export default class Weather extends Component {
     this.state = {
         loading: false,
         weatherData: this.data,
-        place : ""
+        place : "Pokhara ",
+        long : "84.07973878182344",
+        lati : "28.16537235"
     }
+  }
+  formatString = (input) =>
+  {
+  let formattedString = input.replace(/[^a-zA-Z0-9]+/g, '%');
+  // Remove '%' from the beginning and end of the string
+  formattedString = formattedString.replace(/^%+/, '');
+  return (formattedString)
   }
 
   async componentDidMount () 
   {
-    let url = "https://api.open-meteo.com/v1/forecast?latitude=28&longitude=120&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min,weather_code"
+    let url = `https://api.open-meteo.com/v1/forecast?latitude=${this.state.lati}&longitude=${this.state.long}&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min,weather_code`
+    console.log(url)
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
         loading : true,
         weatherData : parsedData
     })
-    console.log("LOADING"+JSON.stringify(this.state.weatherData))
+   // console.log("LOADING"+JSON.stringify(this.state.weatherData))
    
   }
+  handleOnChange = async (event) =>{
+    this.setState({
+      place : event.target.value,
+    }, ()=> {console.log(this.formatString(this.state.place))})
+    let formatted = this.formatString(event.target.value)
+    let apiKey = "57540f33e1e04d2b898c165e7470b5f3"
+    console.log(`https://api.geoapify.com/v1/geocode/search?text=${formatted}&format=json&apiKey=${apiKey}`)
+    await fetch(`https://api.geoapify.com/v1/geocode/search?text=${formatted}&format=json&apiKey=${apiKey}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("longlatdata"+data.results[0].lon+data.results[0].lat)
+      this.setState({
+        long : data.results[0].lon,
+        lati : data.results[0].lat
+      })
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    let url = `https://api.open-meteo.com/v1/forecast?latitude=${this.state.lati}&longitude=${this.state.long}&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min,weather_code`
+    console.log(url)
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+        loading : true,
+        weatherData : parsedData
+    })
+ }
+
   render() {
     return (
       <div className="container text-center">
-        <h2>Weather of </h2>
+        <h2>Weather of 
+          <textarea name="" id="" cols="20" rows="1" value={this.state.place} onChange={this.handleOnChange}></textarea>
+        </h2>
         {!this.state.loading && < LoadData data={this.state.weatherData} />}
         {this.state.loading && < LoadData data={this.state.weatherData} />}
       </div>
